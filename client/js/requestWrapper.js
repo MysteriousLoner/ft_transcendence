fdb5c47465a8bdc86f185ca1516205598e83fcee
MysteriousLoner <leeyang031110@gmail.com>
2024-12-24 17:23:36 +0800

    wrapper

---
+++ client/js/requestWrapper.js
@@ -0,0 +1,7 @@
+/**
+ * This file contains wrapper functions I made to handle request to the servers.
+ * Since we use JWT to secure the server, these wrapper funnctionns will handle the token passing.
+ * Standard input should be (apiEndpoint, requestMethod, jsonMessage)
+ * Standard output will always be a json object.
+ */
+
