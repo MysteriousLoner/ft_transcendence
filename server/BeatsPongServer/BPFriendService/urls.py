9527ffe061c49551a54c23523614d0a764059c97
MysteriousLoner <leeyang031110@gmail.com>
2025-01-10 03:36:19 +0800

    getProfileData

--- server/BeatsPongServer/BPFriendService/urls.py
+++ server/BeatsPongServer/BPFriendService/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import getFriendList, addFriend, removeFriend
+from .views import getFriendList, addFriend, removeFriend, getProfileData
 
 urlpatterns = [
     path('getFriendList/', getFriendList, name='getFriendList'),
     path('addFriend/', addFriend, name='addFriend'),
     path('removeFriend/', removeFriend, name='removeFriend'),
+    path('getProfileData/', getProfileData, name='getProfileData'),
 ]
